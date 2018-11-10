4566e861cd4df8fec7cb1e2f51e26159f18da5b3
Misha Behersky <bmwant@gmail.com>
2018-11-10 22:26:31 +0200

    Prettify it :chocolate_bar:

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,21 @@
 $(function() {
   $('#is-contributor').click(function() {
+    $('#results-modal').addClass('in');
+    $('#overlay').show();
     var handle = $('input[name="handle"]').val();
     var repository = $('input[name="repository"]').val();
 
-    $.get('/check_contributor', {
-      handle: handle,
-      repo: repository
-    }, function () {})
-      .always(function (data, status, xhr) {
-        if (xhr.status === 200) {
-          $('#handle-success').css('visibility', 'visible');
-        } else {
-          $('#handle-fail').css('visibility', 'visible');
-        }
-      });
+    // $.get('/check_contributor', {
+    //   handle: handle,
+    //   repo: repository
+    // }, function () {})
+    //   .always(function (data, status, xhr) {
+    //     if (xhr.status === 200) {
+    //       $('#handle-success').css('visibility', 'visible');
+    //     } else {
+    //       $('#handle-fail').css('visibility', 'visible');
+    //     }
+    //   });
   });
   if (false) {
     $.get('/check_handle', {handle: handle}, function () {
